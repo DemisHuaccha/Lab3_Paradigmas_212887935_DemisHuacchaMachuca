1e1eb274cbd464d5fc09f05b19e43a0bcbe07f5a
DemisHuaccha <128428584+DemisHuaccha@users.noreply.github.com>
2024-12-26 17:36:53 -0300

    Update Main.java
    Correcciones por el cambio de nombre de la clase TDAPiece

--- src/main/java/menu/Main.java
+++ src/main/java/menu/Main.java
@@ -108,7 +108,6 @@ public class Main implements verificar {
                 System.out.println(" 1. Crear Jugador");
                 System.out.println(" 2. Ver Jugadores");
                 System.out.println(" 3. Anterior");
-                System.out.println("----- LOS JUGADORES NO DEBEN TENER UN ID O COLOR IGUAL -----");
                 int selectorAux1 = 0;
                 try {
                     System.out.println("Ingrese una opcion: ");
@@ -125,23 +124,12 @@ public class Main implements verificar {
                     System.out.println("Color del Jugador:");
                     String colorJugador;
                     colorJugador = scannerPrincipal.nextLine();
-                    try {
-                        System.out.println("identificador del Jugador:");
-                        input = scannerPrincipal.nextLine();
-                        int identificador = Integer.parseInt(input);
-                        TDAPlayer playeraux = new TDAPlayer(identificador, nombreJugador, colorJugador, 0, 0, 0, 20);
-                        if (app.verificarDuplicados(players, playeraux)) {
-                            players.add(playeraux);
-                        }
-                        else {
-                            System.out.print("Identificador o Color del jugador repetido.");
-                            System.out.println();
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("Debes ingresar un numero entero.");
-                    }
+                    int identificador = players.size()+1;
+                    TDAPlayer playeraux = new TDAPlayer(identificador, nombreJugador, colorJugador, 0, 0, 0, 20);
+                    players.add(playeraux);
 
-                } else if (selectorAux1 == 2) {
+                }
+                else if (selectorAux1 == 2) {
                     int largo = players.size();
                     if (largo < 1) {
                         System.out.println("No hay jugadores creados");
@@ -171,7 +159,7 @@ public class Main implements verificar {
                 }
 
                 if (players.size() < 2) {
-                    System.out.println("No hay suficientes jugadores creados, se necesitan crear 2 o mas jugadores");
+                    System.out.println("No hay suficientes jugadores creados, se necesitan por lo menos 2 jugadores creados");
                     System.out.println();
                 }
                 else if (selectorAux2 == 1) {
@@ -192,18 +180,24 @@ public class Main implements verificar {
                         try {
                             input = scannerPrincipal.nextLine();
                             auxPlayer2 = Integer.parseInt(input);
-                            System.out.println("Cantidad de piezas por jugador: ");
-                            System.out.println("----- 4 <= Cantidad de fichas <= 21 -----");
-                            try {
-                                input = scannerPrincipal.nextLine();
-                                auxPiezas = Integer.parseInt(input);
-                                TDABoard auxboard = new TDABoard();
-                                players.get(auxPlayer1 - 1).setRemainingPieces(auxPiezas);
-                                players.get(auxPlayer2 - 1).setRemainingPieces(auxPiezas);
-                                TDAGame newgame = new TDAGame(players.get(auxPlayer1 - 1), players.get(auxPlayer2 - 1), auxboard, players.get(auxPlayer1 - 1).getId());
-                                juegos.add(newgame);
-                            } catch (NumberFormatException e) {
-                                System.out.println("Debes ingresar un numero entero.");
+                            if(auxPlayer2==auxPlayer1){
+                                System.out.println("Selecione 2 jugadores distintos");
+                            }
+                            else {
+                                System.out.println("Cantidad de piezas por jugador: ");
+                                System.out.println("---- Fichas recomendadas para un juego ----");
+                                System.out.println("----- 10 <= Cantidad de fichas <= 21 -----");
+                                try {
+                                    input = scannerPrincipal.nextLine();
+                                    auxPiezas = Integer.parseInt(input);
+                                    TDABoard auxboard = new TDABoard();
+                                    players.get(auxPlayer1 - 1).setRemainingPieces(auxPiezas);
+                                    players.get(auxPlayer2 - 1).setRemainingPieces(auxPiezas);
+                                    TDAGame newgame = new TDAGame(players.get(auxPlayer1 - 1), players.get(auxPlayer2 - 1), auxboard, players.get(auxPlayer1 - 1).getId());
+                                    juegos.add(newgame);
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Debes ingresar un numero entero.");
+                                }
                             }
                         } catch (NumberFormatException e) {
                             System.out.println("Debes ingresar un numero entero.");
@@ -211,7 +205,6 @@ public class Main implements verificar {
                     } catch (NumberFormatException e) {
                         System.out.println("Debes ingresar un numero entero.");
                     }
-                    //Corregir remainingPieces para juegos distintos
                 }
                 else if (selectorAux2 == 2) {
                     System.out.println("Juegos");
@@ -255,16 +248,15 @@ public class Main implements verificar {
                         auxJuego = Integer.parseInt(input);
 
                         TDABoard auxboard = juegos.get(auxJuego - 1).getBoard();
-                        int fin = 1;
                         int aux;
                         app.verificarTurnos(juegos.get(auxJuego-1).getHistory(), juegos.get(auxJuego-1));
                         if(juegos.get(auxJuego-1).getBoard().entregarGanador()!=0) {
                             System.out.println("Juego Terminado");
                             System.out.println("Id Ganador :"+ juegos.get(auxJuego-1).getBoard().entregarGanador());
                         }
-                        while ( fin==1 && auxboard.entregarGanador() == 0 && !juegos.get(auxJuego-1).isDraw()){
+                        while ( auxboard.entregarGanador() == 0 && !juegos.get(auxJuego-1).isDraw()){
                             System.out.println("Turno del jugador: " + juegos.get(auxJuego - 1).getCurrentPlayer());
-                            System.out.println("Estado del juego (Victorio) : " + juegos.get(auxJuego - 1).getBoard().entregarGanador());
+                            System.out.println("Estado del juego (Victoria) : " + juegos.get(auxJuego - 1).getBoard().entregarGanador());
                             System.out.println(juegos.get(auxJuego - 1).getBoard().toString());
                             System.out.println("Columna donde colocar pieza: ");
                             System.out.println("  0   -   1   -   2   -   3   -   4   -   5   -  6  ");
@@ -311,8 +303,6 @@ public class Main implements verificar {
         scannerPrincipal.close();
     }
 }
-
-
 
 
 
